fcaa9fa75f35d37bdf574be3315d819780d31b92
harmiprasetyo <harmi.prasetyo@gmail.com>
2026-05-08 16:15:50 +0700

    user app

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\UserApp;
+use App\Models\UsersApp as UserApp;
 
 class UserController extends Controller
 {
