9b0b8dff44d238be76ba36b539eb8812cb2a9733
harmiprasetyo <harmi.prasetyo@gmail.com>
2026-05-12 10:55:52 +0700

    update composer

--- app/Http/Controllers/Lplpo/LplpoController.php
+++ app/Http/Controllers/Lplpo/LplpoController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\LplpoService;
 use App\Models\Lplpo\Lplpo;
-use App\Models\master\MasterFaskes;
+use App\Models\Master\MasterFaskes;
 
 class LplpoController extends Controller
 {
