698bfa6d31596917706316a4412d9d2dccb08a47
harmi prasetyo <harmi.prasetyo@gmail.com>
2026-09-22 02:03:02 +0700

    update Seeder DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,10 @@ class DatabaseSeeder extends Seeder
     MasterObatSeeder::class,
 ]);
 
+ $this->call([
+    MasterObatBHPSeeder::class,
+]);
+
         $this->call([
             UsersAppSeeder::class,
         ]);
